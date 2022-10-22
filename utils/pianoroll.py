1ebe537cf12db874af545ea0698cf336fe6f54f6
eri24816 <a931eric@gmail.com>
2022-10-22 23:57:39 +0800

    Fix PianoRoll logic

--- utils/pianoroll.py
+++ utils/pianoroll.py
@@ -50,11 +50,12 @@ import miditoolkit
 from utils import io_util
 
 class PianoRollDataset(Dataset):
-    def __init__(self, data_dir, segment_len = 0, hop_len = 32, max_duration = 32*180, shard=0, num_shards=1):
+    def __init__(self, data_dir, segment_len = 0, hop_len = 32, max_duration = 32*180, shard=0, num_shards=1, max_pieces = inf):
         print(f'Creating dataset {segment_len}')
         self.pianorolls : list[PianoRoll] = []
 
         file_list = list(glob.glob(os.path.join(data_dir,"*.json")))
+        file_list = file_list[:max_pieces]
         for file_path in file_list:
             self.pianorolls.append(PianoRoll.load(file_path))
         
@@ -92,6 +93,21 @@ class PianoRollDataset(Dataset):
     def get_all_piano_rolls(self) -> list[PianoRoll]:
         return [self.get_piano_roll(i) for i in range(len(self))]
 
+class Note:
+    def __init__(self,onset,pitch,velocity,offset=None) -> None:
+        self.onset = onset
+        self.pitch = pitch
+        self.velocity = velocity
+        self.offset = offset
+
+    def __repr__(self) -> str:
+        return f'Note({self.onset},{self.pitch},{self.velocity},{self.offset})'
+
+    def __gt__(self, other):
+        if self.onset == other.onset:
+            return self.pitch > other.pitch
+        return self.onset > other.onset
+
 class PianoRoll:
     '''
     
@@ -117,7 +133,7 @@ class PianoRoll:
                 if tens[t,p] > thres:
                     data["onset_events"].append([t,p+21,int(tens[t,p])])
 
-        return PianoRoll(data,make_pedal=True)
+        return PianoRoll(data)
     
     @ staticmethod
     def from_midi(path):
@@ -129,30 +145,58 @@ class PianoRoll:
         for note in midi.instruments[0].notes:
             note : miditoolkit.Note
             data["onset_events"].append([int(note.start*8/midi.ticks_per_beat),note.pitch,note.velocity])
-        return PianoRoll(data,make_pedal=True)
+        return PianoRoll(data)
 
-    def __init__(self, data : dict, make_pedal = False):
+    def __init__(self, data : dict):
         # [onset time, pitch, velocity]
-        self.notes = data["onset_events"]
-        if len(self.notes) and len(self.notes[0]) == 4:
-            self.notes = [[onset,pitch,vel] for onset,pitch,vel,offset in self.notes]
+        self.notes = [Note(*note) for note in data["onset_events"]]
         self.notes = sorted(self.notes) # ensure the event is sorted by time
 
-        if len(self.notes):
-            time, pitch, vel = self.notes[-1] # get the last note's onset
-            self.duration = time + 16 # extend a bar to ensure the last note is covered completely
-        else:
-            self.duration = 0
-
-        if make_pedal:
-            self.pedal = list(range(0,self.duration,32))
-        else:
+        if "pedal_events" in data:
             # Timestamps of pedal up events. (Otherwise the pedal is always down) 
             self.pedal = data["pedal_events"]
             self.pedal = sorted(self.pedal)
+        else:
+            self.pedal = None
 
-        self.offsets = self.get_offsets_with_pedal()
+        if len(self.notes):
+            self.duration = self.notes[-1].onset + 16 # extend a bar to ensure the last note is covered completely
+        else:
+            self.duration = 0
 
+        self._have_offset = len(self.notes) == 0 or self.notes[0].offset is not None
+    
+    '''
+    ==================
+    Utils
+    ==================
+    '''
+    def _note_data(self):
+        '''
+        generator that yields (onset, pitch, velocity, offset iterator)
+        '''
+        for note in self.notes:
+            yield note.onset, note.pitch, note.velocity, note.offset
+    
+    def get_offsets_with_pedal(self,pedal) -> list[int]:
+        offsets = []
+        next_onset = [inf]*88
+        i = len(pedal)
+        for onset, pitch, vel, _ in reversed(list(self._note_data())): #TODO: handle offsets if there are ones
+            pitch -= 21 # midi number to piano
+            while i > 0 and pedal[i-1] > onset:
+                i -= 1
+            if i == len(pedal):
+                next_pedal_up = self.duration
+            else:
+                next_pedal_up = pedal[i]
+
+            offset = min(next_onset[pitch], next_pedal_up)
+
+            offsets.append(offset)
+            next_onset[pitch] = onset
+        offsets = list(reversed(offsets))
+        return offsets
     '''
     ==================
     Type conversion
@@ -160,6 +204,12 @@ class PianoRoll:
     '''
 
     def save(self,path):
+
+        data = {"onset_events":[],"pedal_events":[]}
+        for note in self.notes:
+            data["onset_events"].append([note.onset,note.pitch,note.velocity])
+        data["pedal_events"] = self.pedal if self.pedal else []
+
         io_util.json_dump({"onset_events":self.notes,"pedal_events":self.pedal},path)
 
     def to_tensor(self, start_time : int = 0, end_time : int = inf, padding = False, normalized = False) -> torch.Tensor:
@@ -176,7 +226,7 @@ class PianoRoll:
         size = [length, 88]
         piano_roll = torch.zeros(size)
 
-        for time, pitch, vel in self.notes:
+        for time, pitch, vel, _ in self._note_data():
             rel_time = time - start_time
             # only contain notes between start_time and end_time
             if rel_time < 0: continue
@@ -188,18 +238,32 @@ class PianoRoll:
             piano_roll = piano_roll/64-1
         return piano_roll
     
-    def to_midi(self,path = None,encode_pedal = True) -> miditoolkit.midi.parser.MidiFile:
+    def to_midi(self,path = None,apply_pedal = True) -> miditoolkit.midi.parser.MidiFile:
         '''
         Convert the pianoroll to a midi file
         '''
         midi = miditoolkit.midi.parser.MidiFile()
         midi.instruments = [miditoolkit.Instrument(program=0, is_drum=False, name='Piano')]
-        notes = [note + [offset] for note, offset in zip(self.notes,self.offsets)]
         midi.tempo_changes.append(miditoolkit.TempoChange(144,0))
-        for  onset, pitch, vel, offset in notes:
-            midi.instruments[0].notes.append(
-                miditoolkit.Note(vel, pitch, int(onset*midi.ticks_per_beat/8), int(offset*midi.ticks_per_beat/8))
-            )
+        if apply_pedal:
+            if self.pedal:
+                pedal = self.pedal
+            else:
+                pedal = list(range(0,self.duration,32))
+            offsets = self.get_offsets_with_pedal(pedal)
+            for i, (onset, pitch, vel, _) in enumerate(self._note_data()):
+                offset = offsets[i]
+                midi.instruments[0].notes.append(
+                    miditoolkit.Note(vel, pitch, int(onset*midi.ticks_per_beat/8), int(offset*midi.ticks_per_beat/8))
+                )
+        if not apply_pedal:
+            assert self._have_offset, "Offset not found"
+            for onset, pitch, vel, offset in self._note_data():
+                assert offset is not None, "Offset not found"
+                midi.instruments[0].notes.append(
+                    miditoolkit.Note(vel, pitch, int(onset*midi.ticks_per_beat/8), int(offset*midi.ticks_per_beat/8))
+                )
+
         if path:
             midi.dump(path)
         return midi
@@ -217,52 +281,37 @@ class PianoRoll:
         length = end_time - start_time
         sliced_notes = []
         sliced_pedal = []
-        for time, pitch, vel in self.notes:
+        for time, pitch, vel, offset in self._note_data():
             rel_time = time - start_time
             # only contain notes between start_time and end_time
             if rel_time < 0: continue
             if rel_time >= length : break
-            sliced_notes.append([rel_time,pitch,vel])
+            sliced_notes.append([rel_time,pitch,vel,offset])
 
-        for pedal in self.pedal:
-            time = pedal
-            rel_time = time - start_time
-            # only contain pedal between start_time and end_time
-            if rel_time < 0: continue
-            if rel_time >= length : break
-            sliced_pedal.append(rel_time)
-
-        return PianoRoll({"onset_events":sliced_notes,"pedal_events":sliced_pedal})
+        if self.pedal:
+            for pedal in self.pedal:
+                time = pedal
+                rel_time = time - start_time
+                # only contain pedal between start_time and end_time
+                if rel_time < 0: continue
+                if rel_time >= length : break
+                sliced_pedal.append(rel_time)
+            return PianoRoll({"onset_events":sliced_notes,"pedal_events":sliced_pedal})
+        else:
+            return PianoRoll({"onset_events":sliced_notes})
 
     def get_random_tensor_clip(self,duration,normalized = False):
+        '''
+        Get a random clip of the pianoroll
+        '''
         start_time = random.randint(0,(self.duration-duration)//32)*32
         return self.to_tensor(start_time, start_time+duration, normalized = normalized)
 
-    def get_offsets_with_pedal(self):
-        offsets = []
-        next_onset = [inf]*88
-        i = len(self.pedal)
-        for onset, pitch, vel in reversed(self.notes):
-            pitch -= 21 # midi number to piano
-            while i > 0 and self.pedal[i-1] > onset:
-                i -= 1
-            if i == len(self.pedal):
-                next_pedal_up = self.duration
-            else:
-                next_pedal_up = self.pedal[i]
-
-            offset = min(next_onset[pitch], next_pedal_up)
-
-            offsets.append(offset)
-            next_onset[pitch] = onset
-        offsets = list(reversed(offsets))
-        return offsets
-
-
 
 if __name__ == '__main__':  
-    d = PianoRollDataset('/screamlab/home/eri24816/pianoroll_dataset/data/dataset_1/pianoroll', 2*32)
-    orig = d[150]
+    # test
+    d = PianoRollDataset('/home/eri24816/pianoroll/', 2*32,max_pieces=3)
+    orig = d[0]
     file_name = '/tmp/pianoroll_test.mid'
     PianoRoll.from_tensor(orig,0,normalized=True).to_midi(file_name)
     recons = PianoRoll.from_midi(file_name).to_tensor(normalized=True)
