a4c1b07d9717eb508eb9322dcd06103cd592064c
eri24816 <a931eric@gmail.com>
2022-11-30 23:34:47 +0800

    Enable no dist mode

--- improved_diffusion/dist_util.py
+++ improved_diffusion/dist_util.py
@@ -7,7 +7,12 @@ import os
 import socket
 
 import blobfile as bf
-from mpi4py import MPI
+try:
+    from mpi4py import MPI
+    USE_DIST = True
+except ModuleNotFoundError:
+    USE_DIST = False
+
 import torch as th
 import torch.distributed as dist
 
@@ -22,6 +27,8 @@ def setup_dist():
     """
     Setup a distributed process group.
     """
+    if not USE_DIST:
+        return
     if dist.is_initialized():
         return
 
@@ -45,6 +52,8 @@ def dev():
     """
     Get the device to use for torch.distributed.
     """
+    if not USE_DIST:
+        return th.device("cuda" if th.cuda.is_available() else "cpu")
     if th.cuda.is_available():
         return th.device(f"cuda:{MPI.COMM_WORLD.Get_rank() % GPUS_PER_NODE}")
     return th.device("cpu")
@@ -54,6 +63,8 @@ def load_state_dict(path, **kwargs):
     """
     Load a PyTorch file without redundant fetches across MPI ranks.
     """
+    if not USE_DIST:
+        return th.load(path, **kwargs)
     if MPI.COMM_WORLD.Get_rank() == 0:
         with bf.BlobFile(path, "rb") as f:
             data = f.read()
@@ -67,6 +78,8 @@ def sync_params(params):
     """
     Synchronize a sequence of Tensors across ranks from rank 0.
     """
+    if not USE_DIST:
+        return
     for p in params:
         with th.no_grad():
             dist.broadcast(p, 0)
